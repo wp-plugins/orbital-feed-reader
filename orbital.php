237e32df382454bcd674b1082f377383f4df348c
mttktz <mttktz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-07 22:38:36 +0000

    fixing activation hook
    git-svn-id: https://plugins.svn.wordpress.org/orbital-feed-reader/trunk@737509 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- orbital.php
+++ orbital.php
@@ -197,8 +197,8 @@ function plugin_trigger_check() {
   }
 }
 //Turns out you can't just do __FILE__ like it says in the wordpress codex!
-register_activation_hook(WP_PLUGIN_DIR.'/Orbital-Feed-Reader/orbital.php','orbital_activate');
+register_activation_hook(__FILE__,'orbital_activate');
 
-register_uninstall_hook(WP_PLUGIN_DIR.'/Orbital-Feed-Reader/orbital.php','orbital_uninstall_db');
+register_uninstall_hook(__FILE__,'orbital_uninstall_db');
 
 ?>
